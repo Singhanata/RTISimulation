fc73f98d4e533a824c9312300f9dd754395422fd
Krongboon Singhanat <dipl.ing.style@gmail.com>
2020-08-02 13:34:21 +0700

    Autoformat pep8

--- voxel.py
+++ voxel.py
@@ -7,11 +7,12 @@ Created on Thu Jul 30 07:25:15 2020
 Class Definition
 """
 from math import floor
-                
+
+
 class Voxel:
     def __init__(self, idx, width, height, ref_pos):
         """
-        
+
 
         Parameters
         ----------
@@ -35,34 +36,35 @@ class Voxel:
         self.width = width
         self.height = height
         self.ref_pos = ref_pos
-    
+
     def getLeftBound(self):
         return self.ref_pos.x
-    
+
     def getRightBound(self):
         return self.ref_pos.x + self.width
-    
+
     def getLowerBound(self):
         return self.ref_pos.y
-    
+
     def getUpperBound(self):
         return self.ref_pos.y + self.height
-    
+
     def getLowerLeftCorner(self):
         return [self.ref_pos.x, self.ref_pos.y]
-    
+
     def getUpperLeftCorner(self):
         return [self.ref_pos.x, self.ref_pos.y + self.height]
-    
+
     def getLowerRightCorner(self):
         return [self.ref_pos.x + self.width, self.ref_pos.y]
-    
+
     def getUpperRightCorner(self):
         return [self.ref_pos.x + self.width, self.ref_pos.y + self.height]
-    
+
     def getArea(self):
         return self.width * self.height
-        
+
+
 class VoxelField:
     def __init__(self, voxel_w, voxel_l, n_x, n_y, voxel_mat, ref_pos):
         self.voxels = voxel_w
@@ -71,9 +73,9 @@ class VoxelField:
         self.n_y = n_y
         self.voxel_mat = voxel_mat
         self.ref_pos = ref_pos
-        
-    def findVoxelIdx(self, x,y):
-        col = floor((x - self.ref_pos.x)/self.voxel_w)
-        row = floor((y - self.ref.pos.y)/self.voxel_l)
+
+    def findVoxelIdx(self, x, y):
+        col = floor((x - self.ref_pos.x) / self.voxel_w)
+        row = floor((y - self.ref.pos.y) / self.voxel_l)
         idx = col * self.n_y + row
         return idx
